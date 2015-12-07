f9b0eb35a0516f225fa98ca5805958236998a7f2
Dylan Gijsbertsen <d.b.gijsbertsen@live.nl>
2015-12-07 14:26:19 +0100

    unity project

---
+++ Scream For Your Life/Assets/resources/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public string url = "http://localhost:8080/RESTFullDataBase/webresources/com.screamforyourlife.data";
+
+    int id = 7, highscore = 34,  decibel = 78;
+    string username = "Jan";
+	// Use this for initialization
+    void Awake()
+    {
+        
+        
+    }
+
+	void Start () {
+
+        WWWForm form = new WWWForm();
+        form.AddField("id", id);
+        form.AddField("username", username);
+        form.AddField("highscore", highscore);
+        form.AddField("decibel", decibel);
+
+
+        WWW www = new WWW(url, form);
+
+        StartCoroutine(Get());
+        StartCoroutine(WaitForRequest(www));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator Get()
+    {
+        WWW www = new WWW(url);
+        yield return www;
+        Debug.Log(www.text);
+    }
+
+    IEnumerator WaitForRequest(WWW www)
+    {
+        yield return www;
+
+        if (www.error == null)
+        {
+            Debug.Log("WWW ok: " + www.data);
+        }
+        else
+        {
+            Debug.Log("WWW error: " + www.error);
+        }
+    }
+}
