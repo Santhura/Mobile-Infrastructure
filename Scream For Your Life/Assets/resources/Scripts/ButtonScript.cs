dbab3f1070779ee3f192ad686e93614ee21c1f22
June Rademaker <June.Rademaker@hva.nl>
2015-12-29 17:32:40 +0100

    De knoppen werken ten minste

---
+++ Scream For Your Life/Assets/resources/Scripts/ButtonScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour
+{
+    public string Level;
+
+    public void LoadLevel()
+    {
+        Application.LoadLevel(Level);
+    }
+
+}
---
+++ Scream For Your Life/Assets/resources/Scripts/HighscoreScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Item
+{
+    private string name;
+    private double highscore; 
+}
+
+public class HighscoreScript : MonoBehaviour {
+
+    public List<Item> itemList;     //List for showing highscore as an item
+    public string url = "http://localhost:8080/RESTFullDataBase/webresources/com.screamforyourlife.data";     //Url for retrieving data
+
+    // Use this for initialization
+    void Start () {
+        PopulateList();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void PopulateList()
+    {
+        StartCoroutine(RequestAllHighScores());
+
+    }
+
+    IEnumerator RequestAllHighScores()
+    {
+        WWW request = new WWW(url);
+        yield return request;
+    }
+}
