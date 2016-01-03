d2da961fb06e319499a43997f952f3bf8c00a5e6
Dylan Main PC <d.b.gijsbertsen@live.nl>
2016-01-03 18:24:57 +0100

    spawn script changes

--- Scream For Your Life/Assets/resources/Scripts/PlatformSpawnScript.cs
+++ Scream For Your Life/Assets/resources/Scripts/PlatformSpawnScript.cs
@@ -6,14 +6,17 @@ public class PlatformSpawnScript : MonoBehaviour {
     public GameObject platformPrefab;               // prefab of a platform
     public Transform playerPos;                     // players position
 
-    private float minX = -6, maxX = 6;
+    private float minX = -6, maxX = 7;
 
     private Vector2 playerOldPos;
 
     public bool canSpawn;
 
     private int amountSpawn;
+    private float speed = 2;
+    public float spawnTimer = 5;
 
+    GameObject platform;
 	// Use this for initialization
 	void Start () {
         canSpawn = false;
@@ -25,25 +28,29 @@ public class PlatformSpawnScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        spawnTimer -= Time.deltaTime;
         if (!canSpawn)
         {
-            if (playerPos.position.y <= playerOldPos.y)
+            amountSpawn = 0;
+            if (spawnTimer > 0 && amountSpawn == 0)
             {
-                amountSpawn = 0;
                 canSpawn = true;
             }
         }
         else
         {
-            if (playerPos.position.y > playerOldPos.y && amountSpawn < 1)
+            if (amountSpawn < 1 && spawnTimer < 1 && canSpawn)
             {
                 amountSpawn++;
-                Instantiate(platformPrefab, new Vector3(Random.Range(minX, maxX), playerPos.position.y + 5, 0), Quaternion.identity);
-                playerOldPos = playerPos.position;
+                spawnTimer = 5;
+                platform = Instantiate(platformPrefab, new Vector3(Random.Range(minX, maxX), playerPos.position.y + 15, 0), Quaternion.identity) as GameObject;
                 canSpawn = false;
             }
         }
-        //Debug.Log("player pos: " + playerPos.position.y + "\nplayer old pos: " + playerOldPos.y);
-
+        if (platform != null)
+        {
+            platform.transform.Translate(Vector2.down * speed* Time.deltaTime);
+            
+        }
 	}
 }
